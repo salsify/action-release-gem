0929d04f36b308f2076b127d907e1e9e814fda02
Kyle Smith <kyle.smith@salsify.com>
2021-06-23 15:29:21 -0400

    Add logging and handle some edge cases.

--- bundler_utils.rb
+++ bundler_utils.rb
@@ -3,12 +3,18 @@ require 'rest-client'
 require 'yaml'
 
 def with_gem_auth(is_private, &block)
-  is_private ? with_gems_salsify_auth(&block) : with_ruby_gems_auth(&block)
+  if is_private
+    puts "Using gems.salsify.com authentication."
+    with_gems_salsify_auth(&block)
+  else
+    puts "Using rubygems.org authentication."
+    with_ruby_gems_auth(&block)
+  end
 end
 
 def with_ruby_gems_auth
   with_credentials_file do |creds_file|
-    api_key = ENV['RUBYGEMS_API_KEY']
+    api_key = ENV['RUBYGEMS_API_KEY'] or exit_with_error("RUBYGEMS_API_KEY not set!")
     File.write(creds_file, { rubygems_api_key: api_key }.to_yaml)
 
     yield
@@ -17,15 +23,15 @@ end
 
 def with_gems_salsify_auth
   with_credentials_file do |creds_file|
-    auth = ENV['ARTIFACTORY_AUTH_STRING']
+    auth = ENV['ARTIFACTORY_AUTH_STRING'] or exit_with_error("ARTIFACTORY_AUTH_STRING not set!")
     api_key = RestClient.get("https://#{auth}@gems.salsify.com/api/v1/api_key").body
     File.write(creds_file, { gems_salsify_com: api_key }.to_yaml)
 
-    `bundle config gems.salsify.com #{auth}`
+    system('bundle config gems.salsify.com #{auth}', exception: true)
 
     yield
   ensure
-    `bundle config gems.salsify.com`
+    system('bundle config gems.salsify.com')
   end
 end
 
@@ -43,9 +49,9 @@ ensure
 end
 
 def release_gem
-  system('bundle install --jobs=4 --retry=3')
+  system('bundle install --jobs=4 --retry=3', exception: true)
 
   as_git_user(name: 'github-actions', email: 'github-actions@user.noreply.github.com') do
-    system('bundle exec rake release')
+    system('bundle exec rake release', exception: true)
   end
 end
